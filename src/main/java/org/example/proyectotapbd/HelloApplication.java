44240a5ec777aab27c06b4792dfcf8de051962d9
Cherrisita <juangelrt21@gmail.com>
2025-06-01 22:06:19 -0600

    Creacion interfaz base

--- src/main/java/org/example/proyectotapbd/HelloApplication.java
+++ src/main/java/org/example/proyectotapbd/HelloApplication.java
@@ -2,10 +2,6 @@ package org.example.proyectotapbd;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.example.proyectotapbd.modelos.Conexion;
@@ -14,24 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application
 {
     Scene scene;
-    VBox vbox;
-    Menu menu_competencia1;
-    MenuBar menu_bar;
-    MenuItem menu_item_calculadora, menu_item_restaurante, menu_item_rompecabezas;
-    Button btn;
 
     private void create_ui()
     {
-        menu_item_calculadora = new MenuItem("Calculadora");
-        menu_item_restaurante = new MenuItem("Restaurante");
-        menu_item_rompecabezas = new MenuItem("Rompecabezas");
-        menu_competencia1 = new Menu("Competencia 1");
-        menu_competencia1.getItems().addAll(menu_item_calculadora, menu_item_restaurante, menu_item_rompecabezas);
-        menu_bar = new MenuBar();
-        menu_bar.getMenus().addAll(menu_competencia1);
-        vbox = new VBox(menu_bar);
-        scene = new Scene(vbox);
-
+        scene = new Scene(new VBox(), 800, 600);
     }
 
     @Override
@@ -41,7 +23,6 @@ public class HelloApplication extends Application
         create_ui();
         stage.setScene(scene);
         stage.show();
-        stage.setMaximized(true);
     }
 
     public static void main(String[] args) {
